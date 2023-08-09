13d8528891554f143261ad44c8c8119836736fbd
DedDirectora2277 <141119365+DedDirectora2277@users.noreply.github.com>
2023-08-09 18:34:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 menu.onclick = function myMenu(){
+    // Присваивание элемнта по ID
     var x = document.getElementById('myTopnav');
 
+    // Проверка класса элемента
     if(x.className === 'topnav'){
+        // Добавление класса
         x.className += ' responsive';
     } else{
         x.className = 'topnav';
